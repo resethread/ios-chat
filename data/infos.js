d7b51c76c2071b770e30c6e655960d46da68a50d
resethread <resethread@gmail.com>
2016-05-20 15:47:46 +0200

    1st info

--- data/infos.js
+++ data/infos.js
@@ -1,23 +1,8 @@
 var infos = [
 	{
-		title: 'toto o^sum',
-		link: 'https://strongloop.com/strongblog/an-introduction-to-javascript-es6-modules/',
-		p: 'n easy way to import all of the values from a module with a se’s exports under a si'
-	},
-	{
-		title: 'toto o^sum',
-		link: 'https://strongloop.com/strongblog/an-introduction-to-javascript-es6-modules/',
-		p: 'n easy way to import all of the values from a module with a se’s exports under a si'
-	},
-	{
-		title: 'toto o^sum',
-		link: 'https://strongloop.com/strongblog/an-introduction-to-javascript-es6-modules/',
-		p: 'n easy way to import all of the values from a module with a se’s exports under a si'
-	},
-	{
-		title: 'toto o^sum',
-		link: 'https://strongloop.com/strongblog/an-introduction-to-javascript-es6-modules/',
-		p: 'n easy way to import all of the values from a module with a se’s exports under a si'
+		title: 'Opera intègre un VPN sur sa version developpeur',
+		link: 'http://stacklogs.com/article/26/opera-integre-un-vpn-sur-sa-version-developpeur',
+		p: "Pour ceux qui le connaissent pas, Opera est un navigateur parmi les plus anciens et les moins connus. Le métier d'intégrateur/developpeur front demande de se tenir informé sur l'évolution des navigateurs et leurs parts de marché..."
 	},
 ]
 
